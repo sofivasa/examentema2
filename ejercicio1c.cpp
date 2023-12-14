6e221e49cef8f5fbb10bf54ef51df50fce8be34b
sofivasa <sofivasa@myuax.com>
2023-12-14 12:23:40 +0100

    pedirymostratdatoscybiblioteca

--- ejercicio1c.cpp
+++ ejercicio1c.cpp
@@ -1,4 +1,5 @@
 #include "studioh"
+#include <cstdio>
 
 //Definir estructura estudiante
 struct Estudiante{
@@ -10,3 +11,20 @@ struct Estudiante{
 //Declaracion funcion estudiante en c
 void mostrarEstudiante(Estudiante estudiante);
 
+int main(){
+    //Definir variable de tipo estudiante
+    Estudiante estudiante;
+
+    //Pedir datos al usuario
+    printf("Nombre: ");
+    scanf("%s", &estudiante.nombre);
+    printf("Edad: ");
+    scanf("%d", &estudiante.edad);
+    printf("Promedio: ");
+    scanf("%f", &estudiante.promedio);
+
+    //Mostrar los datos del estudiante
+    mostrarEstudiante(estudiante);
+
+    return 0;
+}
