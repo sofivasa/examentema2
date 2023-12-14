b8a1565b207ddda98cfe220fb442f02cfc972167
sofivasa <sofivasa@myuax.com>
2023-12-14 12:05:38 +0100

    commit1(estrucutraestudiante)

---
+++ ejercicio1.cpp
@@ -0,0 +1,13 @@
+#include "iostream"
+#include "string"
+
+using namespace std;
+
+//Definir estrucitra "estudinate"
+struct Estudiante
+{
+    string nombre;
+    int edad;
+    float promedio;
+
+};
