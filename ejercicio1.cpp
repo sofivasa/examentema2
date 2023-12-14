0e7cc492a1053411b261966055396868ab003b0d
sofivasa <sofivasa@myuax.com>
2023-12-14 12:12:12 +0100

    Update ejercicio1.cpp

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -3,38 +3,9 @@
 
 using namespace std;
 
-//Definir estrucitra "estudinate"
-struct Estudiante
-{
+//Definir estructura estudiante
+struct Estudiante{
     string nombre;
     int edad;
     float promedio;
-
 };
-
-//Funcion mostrar estudiante
-void mostrarEstudiante(Estudiante estudiante)
-{
-    cout << "Nombre: " << estudiante.nombre << endl;
-    cout << "Edad: " << estudiante.edad << endl;
-    cout << "Promedio: " << estudiante.promedio << endl;
-}
-int main()
-{
-    //Definir variable de tipo estudiante
-    Estudiante estudiante;
-
-    //Pedir datos al usuario
-    cout << "Nombre: ";
-    getline(cin, estudiante.nombre);
-    cout << "Edad: ";
-    cin >> estudiante.edad;
-    cout << "Promedio: ";
-    cin >> estudiante.promedio;
-
-    //Mostrar los datos del estudiante
-    mostrarEstudiante(estudiante);
-
-    return 0;
-}
-
